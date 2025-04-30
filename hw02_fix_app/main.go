8fdc1dc74f6ca79185680ce43405fbf3b559bc63
Arkadiy.Ohotnikov <arkadiy.ohotnikov@gmail.com>
2025-04-30 14:00:05 +0300

    finaly_fix

--- hw02_fix_app/main.go
+++ hw02_fix_app/main.go
@@ -5,11 +5,40 @@ import (
 
 	"github.com/fixme_my_friend/hw02_fix_app/printer"
 	"github.com/fixme_my_friend/hw02_fix_app/reader"
+)
+
+func main() {
+	path := "data.json"
+
+	fmt.Printf("Enter data file path (press Enter to use default): ")
+	fmt.Scanln(&path)
+
+	if len(path) == 0 {
+		path = "data.json"
+	}
+
+	staff, err := reader.ReadJSON(path, -1)
+	if err != nil {
+		fmt.Printf("Error reading JSON: %v\n", err)
+		return
+	}
+
+	printer.PrintStaff(staff)
+}
+
+/*
+package main
+
+import (
+	"fmt"
+
+	"github.com/fixme_my_friend/hw02_fix_app/printer"
+	"github.com/fixme_my_friend/hw02_fix_app/reader"
 	"github.com/fixme_my_friend/hw02_fix_app/types"
 )
 
 func main() {
-	var path string = "data.json"
+	var path = "data.json"
 
 	fmt.Printf("Enter data file path: ")
 	fmt.Scanln(&path)
@@ -27,3 +56,4 @@ func main() {
 
 	printer.PrintStaff(staff)
 }
+*/
--- hw02_fix_app/reader/pkg.go
+++ hw02_fix_app/reader/pkg.go
@@ -12,10 +12,48 @@ import (
 func ReadJSON(filePath string, limit int) ([]types.Employee, error) {
 	f, err := os.Open(filePath)
 	if err != nil {
+		return nil, fmt.Errorf("failed to open file: %w", err)
+	}
+	defer f.Close()
+
+	bytes, err := io.ReadAll(f)
+	if err != nil {
+		return nil, fmt.Errorf("failed to read file: %w", err)
+	}
+
+	var data []types.Employee
+
+	err = json.Unmarshal(bytes, &data)
+	if err != nil {
+		return nil, fmt.Errorf("failed to unmarshal JSON: %w", err)
+	}
+
+	// если limit >= 0, обрезаем
+	if limit >= 0 && limit < len(data) {
+		data = data[:limit]
+	}
+
+	return data, nil
+}
+
+/*package reader
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"os"
+
+	"github.com/fixme_my_friend/hw02_fix_app/types"
+)
+
+func ReadJSON(filePath string, limit int) ([]types.Employee, error) {
+	f, err := os.Open(filePath)
+	if err != nil {
 		fmt.Printf("Error: %v", err)
 	}
 
-	byte, err := io.ReadAll(f)
+	bytes, err := io.ReadAll(f)
 	if err != nil {
 		fmt.Printf("Error: %v", err)
 		return nil, nil
@@ -23,9 +61,10 @@ func ReadJSON(filePath string, limit int) ([]types.Employee, error) {
 
 	var data []types.Employee
 
-	err = json.Unmarshal(byte, &data)
+	err = json.Unmarshal(bytes, &data)
 
 	res := data
 
 	return res, nil
 }
+*/
